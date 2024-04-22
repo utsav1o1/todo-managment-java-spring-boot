5ecffbd7a3bcae5f80b84f7f9edc6def7be8797d
Utsav1o1 <utsavkarki244@gmail.com>
2024-04-22 20:44:52 +0545

    initial

---
+++ src/main/java/net/learnjava/todo/TodoManagmentApplication.java
@@ -0,0 +1,13 @@
+package net.learnjava.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagmentApplication.class, args);
+	}
+
+}
